f06635c03576ada113b7c3616e40ee06d219c9c3
Abbassihraf <hraf.abbassi@gmail.com>
2020-07-01 17:28:52 +0100

    Add CDN slick JS library

--- index.php
+++ index.php
@@ -268,5 +268,8 @@
   <!-- JQuery -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+    <!-- Slick JS -->
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
 </body>
 </html>
