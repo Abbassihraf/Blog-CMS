111370fae78e81c296f53ee4836905d32dd8598d
Abbassihraf <hraf.abbassi@gmail.com>
2020-07-02 20:49:50 +0100

    Include header into login page

---
+++ login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Login</title>
+</head>
+<body>
+    
+
+     <!-- header -->
+  <header class="clearfix">
+    <div class="logo">
+      <a href="index.php">
+        <h1 class="logo-text"><span>Awa</span>Inspires</h1>
+      </a>
+    </div>
+    <div class="fa fa-reorder menu-toggle"></div>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="register.php">Sign up</a></li>
+        <li>
+          <a href="login.php">
+            <i class="fa fa-sign-in"></i>
+            Login
+          </a>
+        </li>
+        <!-- <li>
+          <a href="#" class="userinfo">
+            <i class="fa fa-user"></i>
+            Awa Melvine
+            <i class="fa fa-chevron-down"></i>
+          </a>
+          <ul class="dropdown">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#" class="logout">logout</a></li>
+          </ul>
+        </li> -->
+      </ul>
+    </nav>
+  </header>
+  <!-- // header -->
+</body>
+</html>
