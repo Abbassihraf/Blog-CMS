1cf576faaf7f1f884ab6ca4a93a872b0a8056aae
Abbassihraf <hraf.abbassi@gmail.com>
2020-07-02 22:24:59 +0100

    Register page done

--- register.php
+++ register.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
     <title>Register</title>
 </head>
 <body>
-    
+
+
+
 <!-- header -->
   <header class="clearfix">
     <div class="logo">
@@ -42,10 +48,47 @@
       </ul>
     </nav>
   </header>
-  <!-- // header -->
+<!-- // header -->
 
 
 
+<!-- Login -->
+<div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="username" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="email" name="email" required="">
+      <label>Email</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="password" required="">
+      <label>Password</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="passwordConf" required="">
+      <label>Confirm Password</label>
+    </div>
+    <a type="submit" name="login-btn" class="submit" href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Login
+    </a>
+    <p>Or <a href="login.php">Sign In</a></p>
+  </form>
+
+
+<!-- // Login -->
+
+  <!-- JQuery -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="assets/js/script.js"></script>
 
 </body>
 </html>
